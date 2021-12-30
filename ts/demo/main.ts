8270d271f9c3ec9dc6cb321cf8fd9e9d3e6a86c4
GokselKUCUKSAHIN <gokselkucuksahin@gmail.com>
2021-12-30 14:37:00 +0300

    insertMany tested.

--- ts/demo/main.ts
+++ ts/demo/main.ts
@@ -1,12 +1,27 @@
 import {mongoDbDriverFactory} from "moosedb";
 
 class Main {
-  public static async main(): Promise<void> {
+  static async insertOneTest(): Promise<void> {
     const driver = await mongoDbDriverFactory("mongodb://superUser:pass123@10.1.8.88:27017");
     driver.db("test").get("new-table");
-    const response = await driver.insertOne({hello: 1, world: 2});
+    const response = await driver.insertOne({my: 3, file: 4});
     console.log("response:", response);
     console.log("close response:", await driver.close());
+  }
+
+  static async insertManyTest(): Promise<void> {
+    const driver = await mongoDbDriverFactory("mongodb://superUser:pass123@10.1.8.88:27017");
+    driver.db("test").get("new-table");
+    const response = await driver.insertMany([{multiple: 5, doc: 6}, {insert: 7, test: 8}]);
+    console.log("response:", response);
+    console.log("close response:", await driver.close());
+  }
+
+  public static async main(): Promise<void> {
+    // await Main.insertOneTest();
+    // await Main.insertManyTest();
+
+
   }
 }
 
